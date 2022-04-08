16f34a13e8bb360b7b2d95430a3a28957472a5c0
YogaAdityaPrabowo-TI <yoga.aditya.ft19@mail.umy.ac.id>
2022-04-08 15:09:15 +0700

    ktp1

---
+++ src/test/java/PAE/Project/KTP/ApplicationTests.java
@@ -0,0 +1,13 @@
+package PAE.Project.KTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
