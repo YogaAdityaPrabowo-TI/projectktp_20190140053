8eafa8f43e9577fb019edf07169a61937c8eb7df
YogaAdityaPrabowo-TI <yoga.aditya.ft19@mail.umy.ac.id>
2022-04-21 20:46:58 +0700

    next

--- src/main/java/PAE/Project/KTP/DataController.java
+++ src/main/java/PAE/Project/KTP/DataController.java
@@ -4,10 +4,41 @@
  */
 package PAE.Project.KTP;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  *
  * @author HP
  */
+@Controller
 public class DataController {
     
+    DataJpaController datactrl = new DataJpaController();
+    List<Data> newdata = new ArrayList<>();
+    
+    @RequestMapping("/main")
+    public String getMain(){
+        return"menu";
+    }
+    
+    @RequestMapping("/data")
+    public String getDataKTP(Model model){
+        
+        try{
+            newdata = datactrl.findDataEntities(0, 1);
+        }
+        catch (Exception e) {}
+        model.addAttribute("goData", newdata);
+        
+        return "database";
+    }
+    
+    @RequestMapping("/edit")
+    public String doEdit(){
+        return "editktp";
+    }
 }
