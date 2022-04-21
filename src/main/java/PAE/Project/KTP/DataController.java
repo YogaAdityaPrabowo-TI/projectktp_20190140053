2bd3c7e3b0b2914a7e5b7079cf9d308289815563
YogaAdityaPrabowo-TI <yoga.aditya.ft19@mail.umy.ac.id>
2022-04-22 01:33:31 +0700

    ktp 2

--- src/main/java/PAE/Project/KTP/DataController.java
+++ src/main/java/PAE/Project/KTP/DataController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -26,13 +27,17 @@ public class DataController {
     }
     
     @RequestMapping("/data")
+//    @ResponseBody
     public String getDataKTP(Model model){
+        int record = datactrl.getDataCount();
+        String result="";
         
         try{
-            newdata = datactrl.findDataEntities(0, 1);
+            newdata = datactrl.findDataEntities().subList(0, record);
         }
-        catch (Exception e) {}
+        catch (Exception e) {result=e.getMessage();}
         model.addAttribute("goData", newdata);
+        model.addAttribute("record", record);
         
         return "database";
     }
--- src/main/java/PAE/Project/KTP/DataJpaController.java
+++ src/main/java/PAE/Project/KTP/DataJpaController.java
@@ -27,14 +27,14 @@ public class DataJpaController implements Serializable {
     }
     private EntityManagerFactory emf = Persistence.createEntityManagerFactory("PAE_Project.KTP_jar_0.0.1-SNAPSHOTPU");
 
-    public EntityManager getEntityManager() {
-        return emf.createEntityManager();
-    }
-
     public DataJpaController() {
     }
     
     
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
 
     public void create(Data data) throws PreexistingEntityException, Exception {
         EntityManager em = null;
